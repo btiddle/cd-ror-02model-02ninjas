fee6d8a68d87f9ad5254537a2e0380409e0bce41
Benjamin Tiddle <benjamin@intheflow.com>
2014-09-10 09:50:43 -0700

    Initialize repository with completed assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DojoNinjas::Application.config.secret_key_base = 'd51393da602723786f700c5aa3bd0b67fe1b132149faff7a3ed9c7f9b7216dde9ba8630062e76c43d26640beca8715c90517887547c1d18a3b6c735261410a37'
